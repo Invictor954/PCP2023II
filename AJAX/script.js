13f90fba1d1c49e8dcaf60a2091ba53c7759a0ed
Sebastian_Cespedes <sebastian.cespedes1@unmsm.edu.pe>
2023-09-08 13:42:52 -0500

    Agregando texto de errores mas descriptivo

--- AJAX/script.js
+++ AJAX/script.js
@@ -20,3 +20,30 @@ $(document).ready(function() {
         });
     });
 });
+
+// ... (resto del código anterior) ...
+
+$("#obtenerClima").click(function() {
+    let ciudad = $("#ciudad").val();
+
+    if (ciudad) {
+        $.ajax({
+            type: 'GET',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=TU_CLAVE_DE_API&units=metric&lang=es`,
+            success: function(data) {
+                let infoClima = `
+                    Ciudad: ${data.name} <br>
+                    Temperatura: ${data.main.temp}°C <br>
+                    Descripción: ${data.weather[0].description}
+                `;
+
+                $("#clima").html(infoClima);
+            },
+            error: function() {
+                $("#clima").html("Error al obtener la información del clima, Asegurese de escribir correctamente el nombre del País");
+            }
+        });
+    } else {
+        $("#clima").html("Por favor, ingresa una ciudad.");
+    }
+});
